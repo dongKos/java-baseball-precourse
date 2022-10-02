a40b1037919b0b1e809a01fc54555f0e83c73c30
dongko <dongko@hpcnt.com>
2022-10-02 21:13:21 +0900

    feat(controller) : 정답을 맞추면 새로 입력을 받아, 게임을 계속하거나 종료한다.

--- src/main/java/baseball/controller/BaseballController.java
+++ src/main/java/baseball/controller/BaseballController.java
@@ -4,6 +4,8 @@ import baseball.model.Computer;
 import baseball.model.User;
 import baseball.view.GameView;
 
+import static baseball.constant.BaseballProperties.GAME_SIZE;
+
 public class BaseballController {
     private final GameView gameView = new GameView();
     private User user;
@@ -15,7 +17,10 @@ public class BaseballController {
     }
 
     private void play() {
-        guess();
+        while (!user.isCorrect()) {
+            guess();
+        }
+        askRestart();
     }
 
     private void init() {
@@ -30,5 +35,17 @@ public class BaseballController {
         String inputNumber = gameView.getInputNumber();
 
         System.out.println(user.guessAndGetResult(inputNumber, computer.getAnswer()));
+    }
+
+    private void askRestart() {
+        System.out.println(GAME_SIZE + "개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면1, 종료하려면 2를 입력하세요.");
+        String restart = gameView.getInputNumber();
+
+        if(restart.equals("1")) {
+            start();
+        }
+
+        System.exit(0);
     }
 }
